31bbd9f2360e42cf73d2dc435a23e742aff012c9
Remy Skelton <remy.skelton@stu.ocsb.ca>
2025-02-07 16:09:03 +0000

    Done Swift MathOperators

---
+++ MathOperators.swift
@@ -0,0 +1,36 @@
+//
+//  MathOperators.swift
+//
+//  Created by Remy Skelton 
+//  Created on 2025-February-07
+//  Version 1.0
+//  Copyright (c) Remy Skelton. All rights reserved.
+//
+//  This program will display and calculate basic math operations.
+
+import Foundation
+// Float numbers
+let floatOne: Float = 4
+let floatTwo: Float = 6
+// Double numbers
+let doubleOne: Double = 4
+let doubleTwo: Double = 6
+
+// Addition
+print("15 + 7 =", (15 + 7))
+// Subtraction
+print("12 - 8 =", (12 - 8))
+// Multiplication
+print("5 * 3 =", (5 * 3))
+// Division with integers
+print("18 / 6 =", (18 / 6))
+// Division with float numbers
+print("4 / 6 =", (floatOne / floatTwo))
+// Division with double numbers
+print("4 / 6 =", (doubleOne / doubleTwo))
+// Exponents to the power of 2
+print("4^2 =", (pow(4, 2)))
+// Exponents to the power of 3
+print("6^3 =", (pow(6, 3)))
+// Square root
+print("√25 =", (sqrt(25)))
